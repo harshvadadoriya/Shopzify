0376623bea991d74cdb27cf1bb32b01077763e4b
Harsh Vadadoriya <harshvadadoriya079@gmail.com>
2023-05-26 00:25:55 +0530

    added top picks products section by using json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import ColorMode from './colorMode/ColorMode';
-import Banner from './components/banner/Banner';
 import Navbar from './components/navbar/Navbar';
 import TopPicks from './components/products/TopPicks';
 import SecondaryNavbar from './components/secondaryNavbar/SecondaryNavbar';
